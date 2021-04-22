3420def2964c4276142f43443d8e10f7cd686876
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2021-04-22 14:03:25 +0800

    Add more check point before canvas rendering

--- cypress/integration/carta/OpenFitsImage.spec.js
+++ cypress/integration/carta/OpenFitsImage.spec.js
@@ -11,24 +11,41 @@ describe('Open_file_browser', () => {
         cy.visit('')
         cy.get('[class="bp3-button-text"]').contains('Close')
     })
-    it(`Open folder "set_QA"`, ()=>{
+    it(`Open folder "set_QA"`, () => {
         cy.get('input').type('set_QA')
         cy.get(`[title="set_QA"]`).click()
     })
-    it(`Open image "${testImageName}"`, ()=>{
+    it(`Open image "${testImageName}"`, () => {
         cy.get('input').type(testImageName)
         cy.get(`[title="${testImageName}"]`).click()
         const t0 = performance.now()
-        cy.get('[class="bp3-button-text"]').contains('Load').click()
-        // cy.get('[class="bp3-button-text"]', {timeout: 20000}).contains('Load', {timeout: 20000}).should('not.exist').then(()=>{
-        //     cy.wrap(performance.now()).then(t1 => {
-        //         cy.log(`Image load: ${t1 - t0} milliseconds.`);
-        //     })
-        // })
-        cy.get('[class="bp3-label"]', {timeout: 20000}).contains('Clip Min', {timeout: 20000}).then(()=>{
-            cy.wrap(performance.now()).then(t1 => {
-                cy.log(`Image load took ${t1 - t0} milliseconds.`);
+        cy.get('[class="bp3-button-text"]')
+            .contains('Load')
+            .click()
+
+        cy.get('[class="bp3-button-text"]', { timeout: 20000 })
+            .contains('Load', { timeout: 20000 })
+
+        cy.get('[class="bp3-label"]', { timeout: 20000 })
+            .contains('Clip Min', { timeout: 20000 })
+
+        cy.get('input')
+            .get('[type="text"]')
+            .first()
+            .should('have.value', 0) // Clip Min == 0
+
+        cy.get('input')
+            .get('[type="text"]')
+            .first()
+            .invoke('val')
+            .should(text => {
+                const value = parseFloat(text)
+                expect(value).not.to.be.equal(0)
+            }) // Clip Min != 0
+            .then(() => {
+                cy.wrap(performance.now()).then(t1 => {
+                    cy.log(`Image load took ${t1 - t0} milliseconds.`);
+                })
             })
-        })
     })
 })
